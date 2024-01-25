5378f2faee979d76d1f03f0eccceeb6413ed822b
muharremgem <muharremgem28@gmail.com>
2024-01-25 23:55:04 +0300

    İmage upload npm

--- router/main.js
+++ router/main.js
@@ -1,33 +1,36 @@
-const express = require("express");
-const router = express.Router();
-const Post = require("../models/Post");
+const express = require('express')
+const router = express.Router()
+const Post = require('../models/Post')
 
-router.get("/", (req, res) => {
-  res.render("site/index");
+router.get('/', (req, res) => {
+    res.render('site/index')
+})
+
+router.get('/about', (req, res) => {
+    res.render('site/about');
 });
-router.get("/about", (req, res) => {
-  res.render("site/about");
-});
-router.get("/blog", (req, res) => {
-  Post.find({})
-    .lean() //.lean mongoose belgesi" olmaktan çıkıp, basit JavaScript nesnelerine dönüşmesini sağlar.
-    .then((posts) => {
-      res.render("site/blog", { posts: posts });
-      console.log(posts);
+
+router.get('/blog', (req, res) => {
+    Post.find({}).lean().then(posts => {
+        console.log(posts)
+        res.render('site/blog', { posts: posts });
     });
 });
 
-router.get("/post", (req, res) => {
-  res.render("site/post");
-});
-router.get("/contact", (req, res) => {
-  res.render("site/contact");
-});
-router.get("/login", (req, res) => {
-  res.render("site/login");
-});
-router.get("/register", (req, res) => {
-  res.render("site/register");
+router.get('/contact', (req, res) => {
+    res.render('site/contact');
 });
 
-module.exports = router;
+router.get('/post', (req, res) => {
+    res.render('site/post');
+});
+
+router.get('/login', (req, res) => {
+    res.render('site/login');
+});
+
+router.get('/register', (req, res) => {
+    res.render('site/register');
+});
+
+module.exports = router
