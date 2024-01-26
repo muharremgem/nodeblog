58dc822fc7e6e7bcf4a65e268e02035099323375
muharremgem <muharremgem28@gmail.com>
2024-01-26 20:35:38 +0300

    generateDate

--- app.js
+++ app.js
@@ -4,16 +4,25 @@ const exphbs = require("express-handlebars");
 const app = express();
 const port = 3000;
 const hostname = "127.0.0.1";
-const hbs = exphbs.create();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
+const moment = require('moment')
+
 
 mongoose.connect("mongodb://localhost:27017/nodeblog_db");
 
 app.use(fileUpload());
 
 app.use(express.static("public"));
+
+const hbs = exphbs.create({
+  helpers: {
+    generateDate: (date, format) => {
+      return moment(date).format(format);
+    },
+  },
+});
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
